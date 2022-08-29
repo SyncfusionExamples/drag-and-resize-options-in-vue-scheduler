18664931dea90b93ceb6356ab3c9461840fc19b9
Ammu1492 <60737792+Ammu1492@users.noreply.github.com>
2022-08-29 20:40:50 +0530

    Added a sample

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense("Mgo+DSMBMAY9C3t2VVhiQlFaclxJVHxBYVF2R2FJd1RwcV9GYUwgOX1dQl9hSXZRf0VgWXdbdnFQRmY=");
+
+createApp(App).mount('#app')
